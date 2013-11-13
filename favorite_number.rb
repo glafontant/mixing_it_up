36ba672b736707aa1bc445ef16673eacc5521930
Gabe Lafontant <glafonta@gmail.com>
2013-11-13 13:31:27 -0500

    simple program involving gets method

---
+++ favorite_number.rb
@@ -0,0 +1,5 @@
+puts "Hey there, what is your favorite number?"
+favorite_number = gets.chomp.to_i
+new_number = favorite_number + 1
+puts "Here is a suggestion: #{new_number} is a much bigger and better number! 
+Don't you agree?" 
